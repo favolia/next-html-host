491f2ebececf8c514f429000fc536ba7c4a709b1
daisszo1455 <daisszo1455@gmail.com>
2024-06-29 09:56:17 +0000

    fix fs

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,11 +1,16 @@
 "use server";
 import { NextResponse } from "next/server";
 import fs from "fs/promises"
+import path from 'path';
+import short from "short-uuid";
 
 export const GET = async req => {
     try {
         // await fs.mkdir("_BROOO")
         // console.log("_BROOO folder has created")
+        const generateID = short.generate();
+        const uploadDir = path.join('html', generateID);
+        await fs.mkdir(uploadDir, { recursive: true });
         const files = await fs.readdir(process.cwd())
         return NextResponse.json(files, { status: 200 })
     } catch (error) {

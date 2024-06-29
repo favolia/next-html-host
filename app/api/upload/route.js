fc97205a1369b8ec8999b94ba54c824b00209538
daisszo1455 <daisszo1455@gmail.com>
2024-06-29 11:08:53 +0000

    fix fs try

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -18,45 +18,45 @@ export async function POST(req) {
 
         // Generate UUID untuk nama direktori baru
         const generateID = short.generate();
-        const baseDir = process.cwd() + '/public/html/';
+        const baseDir = '/public/html/';
         const uploadDir = baseDir + generateID;
 
-        // try {
-        //     await fs.access(baseDir);
-        // } catch (err) {
-        //     if (err.code === 'ENOENT') {
-        //         // Buat direktori html jika belum ada
-        //         await fs.mkdir(baseDir, { recursive: true });
-        //         console.log(`Directory created: ${baseDir}`);
-        //     } else {
-        //         throw err; // Lempar kesalahan lain
-        //     }
-        // }
+        try {
+            await fs.access(baseDir);
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                // Buat direktori html jika belum ada
+                await fs.mkdir(baseDir, { recursive: true });
+                console.log(`Directory created: ${baseDir}`);
+            } else {
+                throw err; // Lempar kesalahan lain
+            }
+        }
 
-        // console.log(`process.cwd(): ${process.cwd()}`);
-        // console.log(`baseDir: ${baseDir}`);
-        // console.log(`uploadDir: ${uploadDir}`);
+        console.log(`process.cwd(): ${process.cwd()}`);
+        console.log(`baseDir: ${baseDir}`);
+        console.log(`uploadDir: ${uploadDir}`);
 
-        // await fs.mkdir(uploadDir, { recursive: true });
-        // console.log(`Directory created: ${uploadDir}`);
-        // const urls = [];
+        await fs.mkdir(uploadDir, { recursive: true });
+        console.log(`Directory created: ${uploadDir}`);
+        const urls = [];
 
-        // for (const file of files) {
-        //     if (file.type === "text/html") {
-        //         urls.push(`${generateID}/${file.name}`);
-        //         const arrayBuffer = await file.arrayBuffer();
-        //         const buffer = new Uint8Array(arrayBuffer);
-        //         const filePath = `/${uploadDir}/${file.name}`;
-        //         await fs.writeFile(filePath, buffer);
-        //         console.log(`File written: ${filePath}`);
-        //     } else {
-        //         console.log(`Skipped file (not HTML): ${file.name}`);
-        //     }
-        // }
+        for (const file of files) {
+            if (file.type === "text/html") {
+                urls.push(`${generateID}/${file.name}`);
+                const arrayBuffer = await file.arrayBuffer();
+                const buffer = new Uint8Array(arrayBuffer);
+                const filePath = `/${uploadDir}/${file.name}`;
+                await fs.writeFile(filePath, buffer);
+                console.log(`File written: ${filePath}`);
+            } else {
+                console.log(`Skipped file (not HTML): ${file.name}`);
+            }
+        }
 
-        // revalidatePath("/");
+        revalidatePath("/");
 
-        return NextResponse.json({ status: "success", directory: baseDir, uploadDir, urls: null });
+        return NextResponse.json({ status: "success", directory: baseDir, uploadDir, urls });
     } catch (e) {
         console.error(e);
         return NextResponse.json({ status: "fail", error: e.message });

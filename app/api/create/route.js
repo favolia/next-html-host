c9b97fc3ce20f7b1d412febc8fe6650c67d74e6e
daisszo1455 <daisszo1455@gmail.com>
2024-06-30 04:39:32 +0000

    check create

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -9,7 +9,7 @@ export const GET = async req => {
 
     try {
 
-        await fs.writeFile("/db/"+"example.html", "<p>HAI</p>", (err) => {
+        await fs.writeFile("example.html", "<p>HAI</p>", (err) => {
             if (err)
                 console.log(err);
             else {

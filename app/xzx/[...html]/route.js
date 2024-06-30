90a51a1ef95413e4e2a17ec2e232275ef60d30e5
daisszo1455 <daisszo1455@gmail.com>
2024-06-30 05:45:48 +0000

    FIXXXXXXXXXXXXXXXXXXXXXXXXXXX

--- app/xzx/[...html]/route.js
+++ app/xzx/[...html]/route.js
@@ -5,7 +5,7 @@ import { promises as fs } from 'fs';
 /**@param {NextRequest} req */
 export const GET = async req => {
     try {
-        const html = await fs.readFile(`../../tmp${req.url.split("xzx")[1]}`, "utf8")
+        const html = await fs.readFile(`../../tmp${req.url.split("vercel.app/xzx")[1]}`, "utf8")
 
         return new NextResponse(html,
             {
@@ -13,6 +13,6 @@ export const GET = async req => {
             }
         )
     } catch (error) {
-        return NextResponse.json(`File not found ../../tmp${req.url}`, { status: 404 })
+        return NextResponse.json(`File not found`, { status: 404 })
     }
 }
